cc4f11e0b8d6287b96c2565300cc22023ac6f3aa
agnaldo marinho <agnaldomarinho7@gmail.com>
2019-06-10 23:30:55 -0300

    Adicionado security group

---
+++ terraform/networking/sg.tf
@@ -0,0 +1,48 @@
+##Create swarm security group
+resource "aws_security_group" "swarm_sg" {
+  name        = "swarm_sg"
+  description = "Allow all inbound traffic necessary for Swarm"
+  vpc_id      = "${aws_vpc.tf_vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 2377
+    to_port     = 2377
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  tags {
+    Name = "swarm_sg"
+  }
+}
